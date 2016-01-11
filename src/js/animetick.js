cf6c985279f4b5dd9c50ad2f554822d348887d23
otiai10 <otiai10@gmail.com>
2016-01-11 17:20:33 +0900

    Refactor DOM document a bit

--- src/js/animetick.js
+++ src/js/animetick.js
@@ -1,23 +1,29 @@
 var __pt = __pt || {};
 __pt.FormatSourceURL = function(cb){
-    __pt.config.get("sourceURL0", cb);
+  __pt.config.get("sourceURL0", cb);
 };
 __pt.getSourceURL = function(title, cb) {
-    __pt.FormatSourceURL(function(format) {
-        if (!format) return;
-        cb(format.replace(/#{title}/g, title));
-    });
+  __pt.FormatSourceURL(function(format) {
+    if (!format) return;
+    cb(format.replace(/#{title}/g, title));
+  });
 };
 __pt.Anchor = function(title, cb) {
-    var url = __pt.getSourceURL(title, function(url) {
-        cb('<a target="_blank" href="'+url+'" style="text-decoration: none;color: rgba(255,80,20,1);">'+title+'</a>');
-    });
+  var url = __pt.getSourceURL(title, function(url) {
+    var a = document.createElement('a');
+    a.setAttribute('href', url);
+    a.setAttribute('style', 'text-decoration: none;color: rgba(255,80,20,1)');
+    a.setAttribute('target', '_blank');
+    a.innerText = title;
+    cb(a);
+  });
 };
 (function(){
-    var _s = document.getElementsByClassName('anime-info-bottom');
-    if (!_s.length) return;
-    var title = _s[0].getElementsByTagName('h2')[0];
-    __pt.Anchor(title.innerText, function(anchor) {
-        title.innerHTML = anchor;
-    });
+  var _s = document.getElementsByClassName('anime-info-bottom');
+  if (!_s.length) return;
+  var title = _s[0].getElementsByTagName('h2')[0];
+  __pt.Anchor(title.innerText, function(anchor) {
+    title.innerHTML = '';
+    title.appendChild(anchor);
+  });
 })();
